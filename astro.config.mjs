5022c140ad8046f9aa7fe7eed4e0e3fc5fde1898
jacobo <mister.jacobo97@gmail.com>
2025-01-09 20:02:38 +1100

    attempt at fixing github pages routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  site: "https://misterjacobo97.github.io",
+  base: "portfolio",
+  integrations: [tailwind(), solidJs()],
 });
+
